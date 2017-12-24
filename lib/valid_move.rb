a60d7fabc9a0f6ee98c90ff6dde064896d00e26c
Brendan O'Dea <brendan.ode4@gmail.com>
2017-12-24 16:50:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,5 @@
-include Comparable
-
 def valid_move?(board, index)
-  if position_taken?(board, index) && index.between(0,8)
+  if position_taken?(board, index) && index.between?(0,8)
     true
   else
     false
